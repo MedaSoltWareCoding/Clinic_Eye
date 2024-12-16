13be708e7df3228005406528239e3d93e26a2ac8
Rachid <pythoncoding846@gmail.com>
2024-12-16 12:39:49 +0100

    first graphics paitent

---
+++ Medical/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data;
+
+
+namespace Medical
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            
+        }
+        private void OpenTab(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                string tabName = button.Tag.ToString();
+                foreach (TabItem tabItem in MainTabControl.Items)
+                {
+                    if (tabItem.Header.ToString() == tabName)
+                    {
+                        MainTabControl.SelectedItem = tabItem;
+                        break;
+                    }
+                }
+            }
+        }
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        private void PatientsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+    }
+
